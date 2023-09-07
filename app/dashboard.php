3e4d286d18ab0f412348f680b4708c4798976a60
heri <spmisttis@gmail.com>
2023-09-07 09:37:46 +0700

    first commit

---
+++ app/dashboard.php
@@ -0,0 +1,41 @@
+<section class="content">
+      <div class="container-fluid text-center">
+        <div class="card">
+          <div class="card-body">
+        <!-- Small boxes (Stat box) -->
+            <img src="../logo.png" width="200px;">
+            <!-- <div class="card">
+            <div class="card-header">
+                <h2 >Visi & Misi <br> STMIK PAMITRAN KARAWANG</h2>
+              </div>
+              <div class="card-header">
+                <h2 class="text-left">Visi</h2>
+              </div> 
+              <div class="card-header text-left">
+                 <li>Menjadi Perguruan Tinggi Informatika yang unggul dalam menciptakan lulusan yang profesional
+                  di karawang pada tahun 2025</li> 
+              </div> 
+            </div>  
+
+            <div class="card">
+              <div class="card-header">
+                <h2 class="text-left">Misi</h2>
+              </div> 
+              <div class="card-header text-left">
+                 <li>Menyelenggarakan Pendidikan Tinggi di Bidang Teknik Informatika yang berbasis KKNI guna
+                  menghasilkan lulusan profesional
+                 </li> 
+                 <li>Melaksanakan Penelitian yang Relevan di Bidang Informatika guna Menghasilkan Lulusan yang
+                  profesional
+                 </li>
+                 <li>Melaksanakan Pengabdian Kepada Masyarakat yang Bermanfaat, Meningkatan Kesejahteraan
+                  Bagi Masyarakat, dan Menghasilkan Lulusan Profesional
+                 </li>
+                 <li>Menjalin Kemitraan, Kerjasama dengan Dunia Usaha dan Industri guna Menghasilkan Lulusan yang Profesional
+                 </li>
+              </div> 
+            </div> 
+          </div> -->
+        </div>    
+      </div><!-- /.container-fluid -->
+    </section>
---
+++ app/doc/test.php
@@ -0,0 +1,5 @@
+<body>
+ <!-- <?php echo $_POST['id'];?>    -->
+<iframe src="doc/<?php echo $_POST['id'];?>" width="500" height="600"></iframe>
+tes page
+</body>
